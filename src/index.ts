011679016e79dd32c4e9dfb38676a3f761ecf427
Ricardo Pina <pinazelaya@gmail.com>
2022-11-04 20:30:09 -0400

    Add create directory action

--- src/index.ts
+++ src/index.ts
@@ -36,3 +36,10 @@ async function listDirContents(filepath: string) {
     console.error('Error occurred while reading the directory!', error);
   }
 }
+
+function createDir(filepath: string) {
+  if (!fs.existsSync(filepath)) {
+    fs.mkdirSync(filepath);
+    console.log('The directory has been created successfully');
+  }
+}
